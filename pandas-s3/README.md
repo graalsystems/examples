3a39ae3d78b9e11d90bbc5f36682a9a53628ac1a
Vincent Devillers <>
2021-09-22 18:05:11 +0200

    Add example for S3

---
+++ pandas-s3/README.md
@@ -0,0 +1,10 @@
+# Prerequisites
+
+    python3 -m pip install --upgrade pip
+    python3 -m pip install --upgrade build
+    
+    python3 -m pip install pandas boto "s3fs<=0.4"
+
+# Build
+
+    python3 -m build
---
+++ pandas-s3/src/pandas_s3/s3.py
@@ -0,0 +1,31 @@
+import os
+
+import pandas as pd
+
+AWS_BUCKET = os.getenv("AWS_BUCKET")
+AWS_ENDPOINT = os.getenv("AWS_ENDPOINT")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+input = "data.csv"
+output = "results.csv"
+
+books_df = pd.read_csv(
+    f"s3://{AWS_BUCKET}/{input}",
+    storage_options={
+        "key": AWS_ACCESS_KEY_ID,
+        "secret": AWS_SECRET_ACCESS_KEY
+    },
+)
+
+table = books_df.groupby(pd.Grouper(freq='M')).sum()
+
+table.to_csv(
+    f"s3://{AWS_BUCKET}/{output}",
+    index=False,
+    storage_options={
+        "key": AWS_ACCESS_KEY_ID,
+        "secret": AWS_SECRET_ACCESS_KEY
+    },
+)
