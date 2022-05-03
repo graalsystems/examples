e749dfad1ef4f4449746607328eca7f9ded84f6a
tgraff <thomasgraff44@gmail.com>
2022-05-03 15:28:00 +0200

    main modified

--- tensorflow_examples/src/tensorflow_example/__main__.py
+++ tensorflow_examples/src/tensorflow_example/__main__.py
@@ -1,68 +1,7 @@
-import tensorflow_datasets as tfds
-import tensorflow as tf
-from tensorflow.keras import layers, models
-
-
-# Import and preprocess the data
-def make_datasets_unbatched():
-    BUFFER_SIZE = 10000
-
-    # Scaling MNIST data from (0, 255] to (0., 1.]
-    def scale(image, label):
-        image = tf.cast(image, tf.float32)
-        image /= 255
-        return image, label
-
-    datasets, _ = tfds.load(name='mnist', with_info=True, as_supervised=True)
-
-    return datasets['train'].map(scale).cache().shuffle(BUFFER_SIZE)
-
-
-# Create a model for classification purposes
-def build_and_compile_cnn_model():
-    model = models.Sequential()
-    model.add(
-        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10, activation='softmax'))
-
-    model.summary()
-
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    return model
-
+import os
 
 def main():
-
-    # if your GPUs don't support NCCL, replace "communication" with another
-    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy(
-        communication=tf.distribute.experimental.CollectiveCommunication.NCCL)
-
-    BATCH_SIZE_PER_REPLICA = 64
-    BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
-
-    with strategy.scope():
-        ds_train = make_datasets_unbatched().batch(BATCH_SIZE).repeat()
-        options = tf.data.Options()
-        options.experimental_distribute.auto_shard_policy = \
-            tf.data.experimental.AutoShardPolicy.DATA
-        ds_train = ds_train.with_options(options)
-        # Model building/compiling need to be within `strategy.scope()`.
-        multi_worker_model = build_and_compile_cnn_model()
-
-
-    multi_worker_model.fit(ds_train,
-                           epochs=10,
-                           steps_per_epoch=70)
-
+    os.system('python tensorflow_distributed.py')
 
 if __name__ == '__main__':
     main()
