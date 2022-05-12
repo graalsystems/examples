7aa6a9a397ef54335aad77fa0fbfa8e5b93db21f
ThomasGraff <64146530+ThomasGraff@users.noreply.github.com>
2022-05-12 13:31:20 +0200

    Update test.py

--- pytest/test.py
+++ pytest/test.py
@@ -5,7 +5,7 @@ import pytest
 def capital_case(x):
     if not isinstance(x, str):
         raise TypeError('Please provide a string argument')
-      return x.capitalize()
+    return x.capitalize()
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
